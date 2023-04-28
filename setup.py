ec2e1a4a5e8a5483cd7e8e6afb640f02acea0fb4
Florian Bordes <florian.bordes@umontreal.ca>
2023-04-28 18:53:43 -0400

    Remove vissl dependency

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup
 setup(
     name="guided-diffusion",
     py_modules=["guided_diffusion"],
-    install_requires=["blobfile>=1.0.5", "torch", "tqdm", "numpy", "torchvision", "faiss-gpu", "vissl"],
+    install_requires=["blobfile>=1.0.5", "torch", "tqdm", "numpy", "torchvision", "faiss-gpu"],
 )
